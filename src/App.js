ace25c6efc9badc268b00e5162dd49c606f2e0f2
XxPlayerxX <ramankhadgi0@gmail.com>
2021-03-18 15:25:41 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './First';
+// ./app.js and first.js are in same location
+import Second from './Second';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello World</h1>
+      <First/> */}
+      {/* jsx syntax */}
+      {/* <Second/> */}
+      <NavBar/>
     </div>
   );
 }
