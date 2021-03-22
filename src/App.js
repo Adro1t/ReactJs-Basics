8cb3761d89c8af0ef6fca8ca055af2cc29ce414c
XxPlayerxX <ramankhadgi0@gmail.com>
2021-03-22 15:37:39 +0545

    sixth commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import First from './First';
-// ./app.js and first.js are in same location
-import Second from './Second';
-import NavBar from './components/NavBar';
-import Carousel from './components/Carousel';
-import Products from './components/Products';
-import Footer from './components/Footer';
+import React from 'react'
+import Routes from './Routes';
 
-function App() {
+const App=()=> {
   return (
-    <div className="App">
-      <NavBar/>
-      <Carousel/>
-      <Products/>
-      <Footer/>
-    </div>
-  );
+      <>
+        <Routes/>
+      </>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,10 @@ import Products from './Products'
 function Home() {
     return (
         <>
-            <NavBar/>
-            <Carousel/>
-            <Products/>
-            <Footer/> 
+            <NavBar />
+            <Carousel />
+            <Products />
+            <Footer />
         </>
     )
 }
