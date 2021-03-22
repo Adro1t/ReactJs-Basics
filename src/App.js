ac4cc4f2491987631b6982ab13d4f1f500d82caa
XxPlayerxX <ramankhadgi0@gmail.com>
2021-03-22 14:52:39 +0545

    fourth commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import First from './First';
 // ./app.js and first.js are in same location
 import Second from './Second';
 import NavBar from './components/NavBar';
+import Carousel from './components/Carousel';
+import Products from './components/Products';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       {/* jsx syntax */}
       {/* <Second/> */}
       <NavBar/>
+      <Carousel/>
+      <Products/>
+      <Footer/>
     </div>
   );
 }
