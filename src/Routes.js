2b059c4cd884199686172dc69e775c2fbc3e467d
XxPlayerxX <ramankhadgi0@gmail.com>
2023-09-21 17:56:37 +0545

    necessary changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Contact from './components/Contact';
-import Deals from './components/Deals';
-import Home from './components/Home';
-import Offer from './components/Offer';
-import SignIn from './components/user/SignIn'
-import SignUp from './components/user/SignUp';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Contact from "./components/Contact";
+import Deals from "./components/Deals";
+import Home from "./components/Home";
+import Offer from "./components/Offer";
+import SignIn from "./components/user/SignIn";
+import SignUp from "./components/user/SignUp";
 
 const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/offer" component={Offer} />
-                <Route exact path="/signup" component={SignUp} />
-                <Route exact path="/signin" component={SignIn} />
-                <Route exact path="/deals" component={Deals} />
-                <Route exact path="/contact" component={Contact} />
-            </Switch>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/offer" component={Offer} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/deals" component={Deals} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default Routes
+export default Routes;
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,7 +22,7 @@ const ContactForm = () => {
 
                         <div className="col-12 mb-3">
                             <label htmlFor="message">Message</label>
-                            <textarea className="form-control" placeholder="Your Message..." rows="10"></textarea>
+                            <textarea className="form-control" placeholder="Your Message..." rows="8"></textarea>
                         </div>
 
                         <div className="col-8 mt-2 offset-md-2 ">
